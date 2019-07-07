9a734a2cbd553d8d77f1aa584408829cf436f997
fritz-c <4413963+fritz-c@users.noreply.github.com>
2019-07-07 19:48:20 +0900

    Updates

---
+++ precache-manifest.e395ed9a8c955c4bebf643fb6a3045e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd4113a4a60021782439c70a8c06fb4",
+    "url": "/bounding-box-labeler/index.html"
+  },
+  {
+    "revision": "6cd31c5bb9bfbf34b9b9",
+    "url": "/bounding-box-labeler/static/css/main.24c70b7a.chunk.css"
+  },
+  {
+    "revision": "3912ec30daa6e0eb7e25",
+    "url": "/bounding-box-labeler/static/js/2.27f0c0f0.chunk.js"
+  },
+  {
+    "revision": "6cd31c5bb9bfbf34b9b9",
+    "url": "/bounding-box-labeler/static/js/main.d5584125.chunk.js"
+  },
+  {
+    "revision": "6b3aff4713bed280d8cb",
+    "url": "/bounding-box-labeler/static/js/runtime~main.105a057a.js"
+  }
+]);
